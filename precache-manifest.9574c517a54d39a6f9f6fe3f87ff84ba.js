518ad7bc23f0dde07311670f3ee9bd99103eb8fa
JorgeRinaldi1995 <57101651+JorgeRinaldi1995@users.noreply.github.com>
2019-11-24 15:07:42 -0200

    primeiro commit

---
+++ precache-manifest.9574c517a54d39a6f9f6fe3f87ff84ba.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f76634c790f5b9a9261bae87d3ef6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9da82242dbdfc5033b2d",
+    "url": "/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "d1f78279ba82b7ecddf0",
+    "url": "/static/css/main.ca64c935.chunk.css"
+  },
+  {
+    "revision": "9da82242dbdfc5033b2d",
+    "url": "/static/js/2.8cec3e9f.chunk.js"
+  },
+  {
+    "revision": "d1f78279ba82b7ecddf0",
+    "url": "/static/js/main.9ebe07ed.chunk.js"
+  },
+  {
+    "revision": "8541b9234d3df4449ad7",
+    "url": "/static/js/runtime-main.07da6aea.js"
+  },
+  {
+    "revision": "14201eb066aa950790d10cb79e7bde0b",
+    "url": "/static/media/MINIDomPedroII.14201eb0.png"
+  },
+  {
+    "revision": "558d32d9960a739f78e5cb980c133e14",
+    "url": "/static/media/MINIMARIOCHET.558d32d9.png"
+  },
+  {
+    "revision": "7f87442dfe83eeca0cd1ff440924eae1",
+    "url": "/static/media/MINIdompedro11estatua.7f87442d.png"
+  },
+  {
+    "revision": "4cc18508dbe1ed77819b7460ae27aac0",
+    "url": "/static/media/MINIluizgamaestampa.4cc18508.png"
+  },
+  {
+    "revision": "295b2e778c7738de8cfda8219a1f6ec9",
+    "url": "/static/media/MINIprincesaisabel.295b2e77.png"
+  },
+  {
+    "revision": "6a00f7cc4ec9c56b9cf2fd89d954c42b",
+    "url": "/static/media/MINIprincipesamurai.6a00f7cc.png"
+  },
+  {
+    "revision": "555a4970e9841d2537f6afc5bf2320d0",
+    "url": "/static/media/escritorio.555a4970.png"
+  },
+  {
+    "revision": "00268388bf0d67a5a619b7f9af673a3f",
+    "url": "/static/media/eu.00268388.png"
+  },
+  {
+    "revision": "3fc6059772886b94e838f68761de71bd",
+    "url": "/static/media/euTrabalhador.3fc60597.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
